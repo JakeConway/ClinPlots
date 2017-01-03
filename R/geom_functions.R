40ee9d9065774b9efdc6ccc501f90a00e55c0cdc
JakeConway <jakeconway45@gmail.com>
2017-01-02 19:37:52 -0500

    make cohort count plot, and move y-scale to right

--- R/geom_functions.R
+++ R/geom_functions.R
@@ -1,10 +1,33 @@
 addLines <- function(plot, line_data) {
-  plot <- plot + geom_line(data = line_data, aes(x = x, y = y, group = y))
+  plot <-
+    plot + geom_line(data = line_data, aes(x = x, y = y, group = y))
   return(plot)
 }
 
-addPoints <- function(plot, data){
+addPoints <- function(plot, data) {
   plot <- (plot + geom_point(data = data, aes(x = avg, y = disorder))
-  + scale_y_discrete())
+           + scale_y_discrete(position = 'right'))
+  return(plot)
+}
+
+addCountText <- function(plot, data) {
+  data$x <- 1
+  plot <-(plot + geom_text(data = data, aes(x = x, y = disorder, label = n))
+      + ggtitle('n')
+      + theme(
+        panel.background = element_rect(fill = 'white', colour = 'white'),
+        plot.background = element_rect(fill = 'white', colour = 'white'),
+        plot.title = element_text(hjust = 0.5),
+        axis.title.y = element_blank(),
+        axis.title.x = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank()
+      )
+    )
+  return(plot)
+}
+
+addHeaderLine <- function(plot, data) {
+  plot <- plot + annotate('segment', x = 0, xend = 2, y = nrow(data)+1, yend = nrow(data)+1)
   return(plot)
 }
--- R/plots.R
+++ R/plots.R
@@ -1,8 +1,14 @@
 DisorderCohortRiskPlot <- function(data){
-  data <- addYposition(data)
+  data$disorder <- as.character(data$disorder)
+  data <- data[order(data$disorder), ]
   line_data <- flattenToX(data)
   plot <- ggplot()
   plot <- addLines(plot, line_data)
   plot <- addPoints(plot, data)
-  plot
+  print(plot)
+
+  text_plot <- ggplot()
+  text_plot <- addCountText(text_plot, data)
+  text_plot <- addHeaderLine(text_plot, data)
+  print(text_plot)
 }
