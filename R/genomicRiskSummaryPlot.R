42f173d84dc90506b8289d3c76f65cb2d87762f5
JakeConway <jakeconway45@gmail.com>
2017-01-03 03:55:23 -0500

    remove print statement

--- R/genomicRiskSummaryPlot.R
+++ R/genomicRiskSummaryPlot.R
@@ -2,7 +2,6 @@ genomicRiskSummaryPlot <- function(data) {
   data <- colsToCharacter(data)
   columns <- colnames(data)
   data <- adjustGeneLabels(data, columns)
-  print(data$studies)
   data <- initGgplots(data, columns)
   drawGenomicRiskSummary(data)
 }
