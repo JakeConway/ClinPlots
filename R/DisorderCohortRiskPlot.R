65fae7f642eae48524e6c5fe0426c97032143afd
JakeConway <jakeconway45@gmail.com>
2017-01-12 15:55:09 -0500

    add package documentation

--- R/DisorderCohortRiskPlot.R
+++ R/DisorderCohortRiskPlot.R
@@ -1,3 +1,16 @@
+#' Function to generate the disorder risk cohort plot of post-test probabilities, as seen in the README.
+#'
+#' @description A function to generate the disorder risk cohort plot, which summarizes the the post-test
+#'              probabilities of each disorder cohort. The disorder risk cohort plot is comprised of a line plot
+#'              that displays the distribution of post-test probabilities for each disorder, along with the
+#'              direction of change in probability. Each line in the plot is aligned with the name of the disorder,
+#'              as well as the number of individuals in the disorder cohort.
+#' @param data A data.frame that contains the columns disorder, n, min, max, avg, and increased. The disorder column
+#'        contains the name of the disorder (character), the n column contains the number of individuals in the disorder
+#'        cohort (integer), the min column contains the minimum post-test probability (numeric), the max columns
+#'        contains the maximum post-test probability (numeric), the avg column contains the average post-test
+#'        probability (numeric), and increased is a column that states if the post-test probability increased (logical).
+#' @export
 disorderCohortRiskPlot <- function(data){
   data <- formatDisorderData(data)
   line_data <- disorderFlattenToX(data)
--- R/genomicRiskSummaryPlot.R
+++ R/genomicRiskSummaryPlot.R
@@ -1,3 +1,12 @@
+#' Function to generate the genomic risk summary plot, as seen in the README.
+#'
+#' @description A function to generate the genomic risk summary plot, which is a series of text plots accompanied
+#' by a line plot displaying the values from one of the columns, usually post-test probability.
+#' @param data A data.frame of the variables to use in the plots. The column that is used for the line plot must
+#'        have one additional value than the other columns. The names of the columns in the data.frame will be
+#'        used for the names of the columns in the plot.
+#' @param column The name or index of the column to be used for the line plot.
+#' @export
 genomicRiskSummaryPlot <- function(data, column) {
   data <- colsToCharacter(data)
   columns <- colnames(data)
